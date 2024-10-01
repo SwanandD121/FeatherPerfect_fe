b6724f93e8f21414593ce4751897459353f0614a
Vinay Yadav <vinay.manoj.yadav491@gmail.com>
2024-10-01 22:28:22 +0530

    updated login page logo

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -15,9 +15,9 @@ const Auth = () => {
     <div className="Auth flex items-center justify-center h-screen gap-16 relative ">
         <div className="a-left flex gap-4 items-center justify-center">
             <img src={Logo} alt="" className='h-24 w-24'/>
-            <div className="Webname flex flex-col gap-2 font-bold">
-                <h1 className='font-bold text-3xl  bg-[100%] bg-repeat bg-gradient-to-r from-[#2eaafa]-to-[#1060d7]'>FeatherPerfect</h1>
-                <h6 className='dark:text-white pt-2'>IPF is our Brand Ambassador!</h6>
+            <div className="Webname flex flex-col font-bold">
+            <h1 className='font-bold text-3xl  bg-[100%] bg-repeat bg-gradient-to-r from-[#2eaafa]-to-[#1060d7] leading-normal'>FeatherPerfect</h1>
+            <h6 className='dark:text-white pt-0 mb-1'>IPF is our Brand Ambassador!</h6>
                 <h6 className='text-xs font-extralight dark:text-white/80'>(You guessed it right! It has Perfect Feathers!)</h6>
             </div>
         </div>
