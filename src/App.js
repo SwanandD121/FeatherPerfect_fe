cee1b699532a75682080665df490926a603ada10
Suresh <sureshb9a11460@gmail.com>
2024-10-02 08:29:21 +0530

    login page responsive design

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App overflow-hidden text-gray-800 bg-gray-200 dark:bg-black/90 p-4">
         <div className="blur-3xl absolute bg-blue-200 dark:bg-blue-800/30 w-80 h-56 rounded-full" style={{top: '-18%', right: '1rem'}}></div>
         <div className="blur-3xl absolute bg-blue-200 dark:bg-blue-800/30 w-64 h-56 rounded-full" style={{top: '36%', left: '-8rem'}}></div>
-        <div className="blur-3xl absolute bg-blue-200 dark:bg-blue-800/30 w-64 h-56 rounded-full" style={{top: '85%', right: '21rem'}}></div>
+        <div className="blur-3xl absolute bg-blue-200 dark:bg-blue-800/30 w-64  h-auto rounded-full" style={{top: '85%', right: '21rem'}}></div>
 
         {/* <Home/>
 
