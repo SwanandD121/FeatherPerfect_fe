872758301222343ead615f6e73c6bcd31a78e782
Vedansh Jain <vedansh.code@gmail.com>
2024-10-10 16:03:55 +0530

    Post card enhanced

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,31 +1,93 @@
-import React from 'react'
-import Comment from '../../img/comment-filled.png'
-import Share from '../../img/share-filled.png'
-import Heart from '../../img/heart-filled.png'
-import NotLike from '../../img/heart.png'
+import React, { useState } from 'react';
+import { FaRegCommentDots, FaRegShareSquare, FaHeart, FaRegHeart, FaEllipsisV } from 'react-icons/fa'; // Import icons from react-icons
 
-const Post = ({data}) => {
+const Post = ({ data }) => {
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpened(!menuOpened);
+  };
+
   return (
-    <div className="Post flex flex-col gap-3  bg-white/70 dark:bg-slate-800 rounded-3xl p-5">
+    <div className="Post flex flex-col gap-3 bg-white/70 dark:bg-slate-800 rounded-3xl p-5 relative">
+      
 
-        <div className="detail flex flex-col ml-2">
-            <span className='dark:text-white'><b>{data.name}</b></span>
-            <span className='dark:text-white/80'> {data.desc}</span>
+      <div className="flex justify-between items-center">
+        <div className="flex items-center">
+         
+          <img
+            src={data.img} 
+            alt="Profile"
+            className="rounded-full w-12 h-12 object-cover mr-3"
+          />
+
+      
+          <div className="detail flex flex-col">
+            <span className="dark:text-white">
+              <b>{data.name}</b>
+            </span>
+           
+            <span className="dark:text-white/80 text-sm">4 hours ago</span> {/* Date of posting */}
+            
+          </div>
+          
+          
         </div>
+        
 
-        <img src={data.img} alt="" className="rounded-lg object-cover   max-h-fi" />
+   
+        <div className="relative">
+          <button className="focus:outline-none" onClick={toggleMenu}>
+            <FaEllipsisV className="text-gray-500 dark:text-white cursor-pointer" /> {/* React Icon for three dots */}
+          </button>
 
-
-        <div className="postReact flex gap-4 hover:cursor-pointer ml-2 ">
-            <img src={data.liked?Heart: NotLike} alt="" className='w-8 h-8'/>
-            <img src={Comment} alt="" className='w-8 h-8' />
-            <img src={Share} alt="" className='w-8 h-8'/>
+       
+          {menuOpened && (
+            <div className="absolute top-8 right-0 bg-white dark:bg-gray-800 shadow-lg rounded-lg w-40 z-50">
+              <ul className="text-gray-700 dark:text-white text-sm">
+                <li className="hover:bg-gray-100 dark:hover:bg-gray-700 px-4 py-2 cursor-pointer">
+                  Not Interested
+                </li>
+                <li className="hover:bg-gray-100 dark:hover:bg-gray-700 px-4 py-2 cursor-pointer">
+                  Unfollow
+                </li>
+                <li className="hover:bg-gray-100 dark:hover:bg-gray-700 px-4 py-2 cursor-pointer">
+                  Report
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
+      </div>
+      <span className="dark:text-white/80 mt-4 text-lg">{data.desc}</span>
+      <img
+        src={data.img}
+        alt=""
+        className="rounded-lg object-contain max-h-96 w-full"
+      />
+      <div className="postReact flex gap-4 hover:cursor-pointer ml-2 mt-2">
+        <button>
+          {data.liked ? (
+            <FaHeart className="size-6 text-red-500" /> 
+          ) : (
+            <FaRegHeart className="size-6 text-white h-8" />           
+          )}
+        </button>
+        <FaRegCommentDots className="size-6 text-gray-100" />           
+        <FaRegShareSquare className="size-6 text-gray-100" />            
+      </div>
 
-        <span className="ml-2 text-sm dark:text-white"><b>{data.likes}</b> Likes </span>
+   
+      <span className="ml-2 text-sm dark:text-white mt-2">
+        Liked by <b>Vedansh</b>, <b>Swanand</b>, and<span className="ml-2 text-sm dark:text-white">
+        <b>{data.likes}</b>
+      </span> <b>{data.moreLikes} more ...</b>
+      </span>
 
+      
+      
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
