ab977dd6100765913efa523d337388062a1bae72
prajwal07 <prajwal.nimbalkar1910@gmail.com>
2024-10-31 10:59:07 +0530

    done with auth

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -45,12 +45,15 @@ const Auth = () => {
     return (
         <div className="Auth flex items-center justify-center h-screen gap-16 relative">
             <div className="a-left flex flex-col lg:flex-row gap-4 items-center justify-center">
-                <div className="items-center justify-center lg:flex-row flex-col flex">
+                <div className="items-center justify-center lg:flex-row flex-col flex select-none">
                     <img src={Logo} alt="" className="h-24 w-24" />
                     <div className="Webname flex flex-col font-bold text-center lg:text-start">
-                        <h1 className="font-bold text-3xl  bg-[100%] bg-repeat bg-gradient-to-r from-[#2eaafa]-to-[#1060d7] leading-normal">
-                            FeatherPerfect
-                        </h1>
+                    <h1 className="text-5xl bg-gradient-to-r from-[#2eaafa] to-[#1060d7] bg-clip-text text-transparent leading-normal Class
+Properties
+select-none">
+                    FeatherPerfect  
+                    </h1>
+
                         <h6 className="dark:text-white pt-0 mb-1">IPF is our Brand Ambassador!</h6>
                         <h6 className="text-xs font-extralight dark:text-white/80">
                             (You guessed it right! It has Perfect Feathers!)
